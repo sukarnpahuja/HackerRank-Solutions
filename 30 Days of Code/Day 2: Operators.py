a9ffac16df2ef4566076a1c64154af103ca63c70
sukarnpahuja <43109006+sukarnpahuja@users.noreply.github.com>
2021-05-28 07:31:40 +0530

    Create Day 2: Operators.py

---
+++ 30 Days of Code/Day 2: Operators.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'solve' function below.
+#
+# The function accepts following parameters:
+#  1. DOUBLE meal_cost
+#  2. INTEGER tip_percent
+#  3. INTEGER tax_percent
+#
+
+def solve(meal_cost, tip_percent, tax_percent):
+    total_cost = 0
+    tip_value = (tip_percent/100)*meal_cost
+    tax_value = (tax_percent/100)*meal_cost
+    total_cost = meal_cost + tax_value + tip_value
+    print(round(total_cost))
+    # Write your code here
+
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+
+    tip_percent = int(input().strip())
+
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
