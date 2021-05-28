8bad4bc6edaa79d314727162e3e34192f71f2d86
sukarnpahuja <43109006+sukarnpahuja@users.noreply.github.com>
2021-05-28 18:20:18 +0530

    Update Triangle Quest.py

--- Python/Math/Triangle Quest.py
+++ Python/Math/Triangle Quest.py
@@ -1,2 +1,5 @@
 for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
     print (((10 ** i - 1) // 9) * i)
+    
+    #we need to get a number that when multiples by i yielded 1,22,333,444. Thus, we needed to have values 1,11,111,1111. (rep-unit)
+    # refer document --> http://www.fact-index.com/r/re/repunit.html
