7a59aa2d670b3a31afb9bc6f135378a27e327d6b
sukarnpahuja <43109006+sukarnpahuja@users.noreply.github.com>
2021-05-28 06:50:14 +0530

    Create Arithmetic Operators.py

---
+++ Python/Introduction/Arithmetic Operators.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    if (a>=1 and a<=10**10):
+        if(b>=1 and b<=10**10):
+            print(a+b)
+            print(a-b)
+            print(a*b)
+        else:
+            print("not valid")
+    else:
+        print("Not valid")
