f1073708dd73e8bb73ea3d03ea956373057ac5e6
sukarnpahuja <43109006+sukarnpahuja@users.noreply.github.com>
2021-05-28 07:10:55 +0530

    Create Set .discard(), .remove() & .pop().py

---
+++ Python/Sets/Set .discard(), .remove() & .pop().py
@@ -0,0 +1,13 @@
+n = int(input())
+s = set(map(int, input().split()))
+N = int(input())
+for i in range(N):
+    c = input().split()
+    if c[0]=='remove':
+        s.remove(int(c[1]))
+    elif c[0]=='discard':
+        s.discard(int(c[1]))
+    else:
+        s.pop()
+
+print(sum(s))
