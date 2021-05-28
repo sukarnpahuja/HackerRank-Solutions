0d3c1e7ade6d293a4cdb09730c5263dc7aeb1595
sukarnpahuja <43109006+sukarnpahuja@users.noreply.github.com>
2021-05-28 07:19:08 +0530

    Create collections.Counter().py

---
+++ Python/Collections/collections.Counter().py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import collections
+
+
+number_of_shoes = int(input())
+sizes_in_stock = collections.Counter(map(int, input().split()))
+
+total_revenue = 0
+
+for _ in range(int(input())):
+    size, price = map(int, input().split())
+    if sizes_in_stock[size]:
+        total_revenue += price
+        sizes_in_stock[size] -= 1
+
+print(total_revenue)
