08882234f437c6525b22108bdf100241a7648ba5
sukarnpahuja <43109006+sukarnpahuja@users.noreply.github.com>
2021-05-28 15:05:07 +0530

    Create Find Angle MBC.py

---
+++ Python/Math/Find Angle MBC.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+
+AB = int(input())
+BC = int(input())
+
+H = math.sqrt(AB**2 + BC**2)
+#M is the midpoint
+H = H/2
+BC_half = BC/2   #forming 90 degrees
+
+#determining the angle by cosine function
+angle = int(round(math.degrees(math.acos(adj/H))))
+
+angle = str(angle)
+
+print(angle+ u"\N{DEGREE SIGN}")
