c44799276032efc2cc64ccd7a0cc45be8894df2d
sukarnpahuja <43109006+sukarnpahuja@users.noreply.github.com>
2021-06-03 11:51:13 +0530

    Create Day 7: Arrays.py

---
+++ 30 Days of Code/Day 7: Arrays.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    A = arr[::-1]
+    for i in A:
+        print(i,end=" ")
